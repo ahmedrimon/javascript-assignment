090bcb7d5a191fb7c1e70e476b8ebbd576a520f7
ahmedrimon <ahmedrimon@gmail.com>
2021-05-18 11:44:57 +0600

    kilometerToMeter

--- assignment.js
+++ assignment.js
@@ -2,8 +2,13 @@
 
 // kilometerToMeter
 
+function kilometerToMeter(kilometer){
+    var meter = kilometer * 1000;
+    return meter;
+}
 
-
+var meter = kilometerToMeter(1)
+console.log(meter);
 
 
 
